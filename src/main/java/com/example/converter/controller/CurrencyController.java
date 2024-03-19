d19bd1c6433183e4138eb7ae39c9ce527ce58b5e
wbryon <tamirbudaev.habibula@gmail.com>
2024-03-19 16:06:59 +0300

    feat: changed from RestTemplate to WebClient

--- src/main/java/com/example/converter/controller/CurrencyController.java
+++ src/main/java/com/example/converter/controller/CurrencyController.java
@@ -2,6 +2,7 @@ package com.example.converter.controller;
 
 import com.example.converter.service.CurrencyService;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
@@ -14,15 +15,27 @@ public class CurrencyController {
         this.currencyService = currencyService;
     }
 
+//    @GetMapping("/list/{base}")
+//    public Map<String, Double> getCurrencyList(@PathVariable String base) {
+//        return currencyService.getCurrencyList(base);
+//    }
+
     @GetMapping("/list/{base}")
-    public Map<String, Double> getCurrencyList(@PathVariable String base) {
-        return currencyService.getCurrencyList(base);
+    public Mono<Map<String, Double>> getCurrencyList(@PathVariable String base) {
+        return currencyService.getCurrencyListMono(base);
     }
 
+//    @GetMapping("/convert")
+//    public Double convertCurrency(@RequestParam String from,
+//                                  @RequestParam String to,
+//                                  @RequestParam Double amount) {
+//        return currencyService.convertCurrency(from, to, amount);
+//    }
+
     @GetMapping("/convert")
-    public Double convertCurrency(@RequestParam String from,
+    public Mono<Double> convertCurrencyMono(@RequestParam String from,
                                   @RequestParam String to,
                                   @RequestParam Double amount) {
-        return currencyService.convertCurrency(from, to, amount);
+        return currencyService.convertCurrencyMono(from, to, amount);
     }
 }
--- src/main/java/com/example/converter/service/CurrencyService.java
+++ src/main/java/com/example/converter/service/CurrencyService.java
@@ -4,8 +4,11 @@ import com.example.converter.entity.CurrencyRates;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import static java.lang.Math.round;
 
@@ -15,11 +18,12 @@ public class CurrencyService {
     @Value("${api.url}")
     private String apiUrl;
 
-
     private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
-    public CurrencyService(RestTemplate restTemplate) {
+    public CurrencyService(RestTemplate restTemplate, WebClient webClient) {
         this.restTemplate = restTemplate;
+        this.webClient = webClient;
     }
 
     public Map<String, Double> getCurrencyList(java.lang.String baseCurrency) {
@@ -50,5 +54,42 @@ public class CurrencyService {
         String currencyApiUrl = String
                 .format("%s/latest?api_key=3UsccLxLgZZIvWHXaZPGMvjWUKljt3P0&base=%s", apiUrl, baseCurrency.toUpperCase());
         return restTemplate.getForObject(currencyApiUrl, CurrencyRates.class);
+    }
+
+//    public Mono<Map<String, Double>> getCurrencyListMono(String baseCurrency) {
+//        return getCurrencyRatesMono(baseCurrency)
+//                .map(CurrencyRates::getRates);
+//    }
+
+    public Mono<Map<String, Double>> getCurrencyListMono(String baseCurrency) {
+        return getCurrencyRatesMono(baseCurrency)
+                .map(currencyRates -> new TreeMap<>(currencyRates.getRates()));
+    }
+
+    public Mono<CurrencyRates> getCurrencyRatesMono(String baseCurrency) {
+        String currencyApiUrl = String
+                .format("%s/latest?api_key=3UsccLxLgZZIvWHXaZPGMvjWUKljt3P0&base=%s", apiUrl, baseCurrency.toUpperCase());
+
+        return this.webClient.get()
+                .uri(currencyApiUrl)
+                .retrieve()
+                .bodyToMono(CurrencyRates.class);
+    }
+
+    public Mono<Double> convertCurrencyMono(String from, String to, Double amount) {
+        return getCurrencyRatesMono(from.toUpperCase())
+                .flatMap(currencyRates -> {
+                    Map<String, Double> rates = currencyRates.getRates();
+                    Double fromRate = rates.get(from.toUpperCase());
+                    Double toRate = rates.get(to.toUpperCase());
+
+                    if (fromRate != null && toRate != null) {
+                        double result = amount * (toRate / fromRate);
+                        return Mono.just(round(result * 100.0) / 100.0);
+                    } else {
+                        return Mono.error(new IllegalArgumentException("Invalid currency code"));
+                    }
+                })
+                .switchIfEmpty(Mono.error(new IllegalArgumentException("Unable to retrieve currency rates")));
     }
 }
